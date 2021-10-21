bd3276ef77f16d1c0e7e5f09fba222fd21b76fee
Parker Cullum <parker.t.cullum@gmail.com>
2021-10-21 08:08:01 -0500

    first commit

--- BonjourEarth/Form1.cs
+++ BonjourEarth/Form1.cs
@@ -19,7 +19,7 @@ namespace BonjourEarth
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Bonjour");
+            MessageBox.Show("Hola");
         }
     }
 }
